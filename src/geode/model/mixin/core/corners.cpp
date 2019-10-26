80f3a2580cf2183a81f725fb14c074561c27f913
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-26 07:56:32 +0200

    Update corners.cpp

--- src/geode/model/mixin/core/corners.cpp
+++ src/geode/model/mixin/core/corners.cpp
@@ -39,18 +39,6 @@ namespace geode
     class Corners< dimension >::Impl
         : public detail::ComponentsStorage< Corner< dimension > >
     {
-    private:
-        friend class bitsery::Access;
-        template < typename Archive >
-        void serialize( Archive& archive )
-        {
-            archive.ext( *this, DefaultGrowable< Archive, Impl >{},
-                []( Archive& archive, Impl& impl ) {
-                    archive.ext( impl,
-                        bitsery::ext::BaseClass< detail::ComponentsStorage<
-                            Corner< dimension > > >{} );
-                } );
-        }
     };
 
     template < index_t dimension >
