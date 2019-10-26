368b176c03c9e7ed04f8295660f81b45f0b4af99
Arnaud Botella <arnaud.botella@geode-solutions.com>
2019-10-26 07:58:11 +0200

    Update model_boundaries.cpp

--- src/geode/model/mixin/core/model_boundaries.cpp
+++ src/geode/model/mixin/core/model_boundaries.cpp
@@ -35,18 +35,6 @@ namespace geode
     class ModelBoundaries< dimension >::Impl
         : public detail::ComponentsStorage< ModelBoundary< dimension > >
     {
-    private:
-        friend class bitsery::Access;
-        template < typename Archive >
-        void serialize( Archive& archive )
-        {
-            archive.ext( *this, DefaultGrowable< Archive, Impl >{},
-                []( Archive& archive, Impl& impl ) {
-                    archive.ext( impl,
-                        bitsery::ext::BaseClass< detail::ComponentsStorage<
-                            ModelBoundary< dimension > > >{} );
-                } );
-        }
     };
 
     template < index_t dimension >
