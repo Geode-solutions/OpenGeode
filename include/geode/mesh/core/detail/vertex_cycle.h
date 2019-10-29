de3c6542abc7493eaa97e1e4b5ed5204515a8764
panquez <pierre.anquez@geode-solutions.com>
2019-10-29 15:47:35 +0100

    fix

--- include/geode/mesh/core/detail/vertex_cycle.h
+++ include/geode/mesh/core/detail/vertex_cycle.h
@@ -46,7 +46,6 @@ namespace geode
                 std::rotate( vertices_.begin(), min_itr, vertices_.end() );
             }
 
-
             VertexCycle( const std::array< index_t, 2 >& vertices )
                 : vertices_{ vertices[0], vertices[1] }
             {
