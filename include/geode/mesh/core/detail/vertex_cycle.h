6fba60225ccf567c1e8ebe225998fc0e6e5b3209
panquez <pierre.anquez@geode-solutions.com>
2019-10-29 15:46:38 +0100

    try fix windows

--- include/geode/mesh/core/detail/vertex_cycle.h
+++ include/geode/mesh/core/detail/vertex_cycle.h
@@ -24,6 +24,9 @@
 #pragma once
 
 #include <geode/basic/common.h>
+
+#include <array>
+#include <algorithm>
 #include <numeric>
 #include <utility>
 #include <vector>
@@ -44,7 +47,7 @@ namespace geode
             }
 
             VertexCycle( const std::array< index_t, 2 >& vertices )
-                : vertices_( std::vector< index_t >{ vertices[0], vertices[1] } )
+                : vertices_{ vertices[0], vertices[1] }
             {
                 auto min_itr =
                     std::min_element( vertices_.begin(), vertices_.end() );
