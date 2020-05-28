fb9e574d3150888eb65a33c7c5b40500566b5ca2
BotellaA <arnaud.botella@geode-solutions.com>
2020-05-28 16:33:57 +0200

    starting

---
+++ include/geode/mesh/builder/surface_mesh_builder.h
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2019 - 2020 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+#include <absl/types/span.h>
+
+#include <geode/basic/factory.h>
+#include <geode/basic/passkey.h>
+
+#include <geode/mesh/builder/vertex_set_builder.h>
+#include <geode/mesh/common.h>
+
+namespace geode
+{
+    FORWARD_DECLARATION_DIMENSION_CLASS( Point );
+    FORWARD_DECLARATION_DIMENSION_CLASS( SurfaceMesh );
+
+    struct PolygonEdge;
+    struct PolygonVertex;
+} // namespace geode
+
+namespace geode
+{
+    /*!
+     * Interface class to represent the builder of a SurfaceMesh
+     */
+    template < index_t dimension >
+    class SurfaceMeshBuilder : public VertexSetBuilder
+    {
+        PASSKEY( SurfaceMesh< dimension >, BuilderKey );
+
+    public:
+        /*!
+         * Create the builder associated with a SurfaceMesh.
+         * @param[in] mesh The SurfaceMesh to build/modify
+         */
+        static std::unique_ptr< SurfaceMeshBuilder< dimension > > create(
+            SurfaceMesh< dimension >& mesh );
+
+        /*!
+         * Set coordinates to a vertex. This vertex should be created before.
+         * @param[in] vertex_id The vertex, in [0, nb_vertices()-1].
+         * @param[in] point The vertex coordinates
+         */
+        void set_point( index_t vertex_id, const Point< dimension >& point );
+
+        /*!
+         * Create a new point with associated coordinates.
+         * @param[in] point The point to create
+         * @return the index of the created point
+         */
+        index_t create_point( const Point< dimension >& point );
+
+        /*!
+         * Create a new polygon from vertices.
+         * @param[in] vertices The ordered vertices defining the polygon to
+         * create
+         * @return the index of the created polygon
+         */
+        index_t create_polygon( absl::Span< const index_t > vertices );
+
+        /*!
+         * Modify a polygon vertex.
+         * @param[in] polygon_vertex The index of the polygon vertex to modify
+         * @param[in] vertex_id Index of the mesh vertex to set as polygon
+         * vertex
+         */
+        void set_polygon_vertex(
+            const PolygonVertex& polygon_vertex, index_t vertex_id );
+
+        /*!
+         * Replace old polygon vertices from a given vertex to another.
+         * @param[in] old_vertex_id Index of the initial mesh vertex to modify
+         * @param[in] new_vertex_id Index of the target mesh vertex to set as
+         * polygon vertex
+         */
+        void replace_vertex( index_t old_vertex_id, index_t new_vertex_id );
+
+        /*!
+         * Set a polygon adjacent through an edge.
+         * @param[in] polygon_edge The index of the polygon edge
+         * @param[in] adjacent_id Index of the adjacent polygon
+         */
+        void set_polygon_adjacent(
+            const PolygonEdge& polygon_edge, index_t adjacent_id );
+
+        /*!
+         * Compute all the adjacencies between the surface polygons
+         */
+        void compute_polygon_adjacencies();
+
+        /*!
+         * Compute the adjacencies between the given surface polygons
+         * @param[in] polygons_to_connect Set of polygons for which compute
+         * adjacencies
+         */
+        void compute_polygon_adjacencies(
+            absl::Span< const index_t > polygons_to_connect );
+
+        /*!
+         * Delete a set of surface polygons
+         * @param[in] to_delete Vector of size polygonal_surface_.nb_polygons().
+         * If to_delete[i] is true the polygon of index i is deleted, else it is
+         * kept.
+         * @return the mapping between old polygon indices to new ones.
+         * Deleted polygons new index is NO_ID
+         */
+        std::vector< index_t > delete_polygons(
+            const std::vector< bool >& to_delete );
+
+        /*!
+         * Delete all the isolated vertices (not used as polygon vertices)
+         * @return the mapping between old vertex indices to new ones.
+         * Deleted vertices new index is NO_ID
+         */
+        std::vector< index_t > delete_isolated_vertices();
+
+        /*!
+         * Delete all the isolated edges (not used as polygon edges)
+         * @return the mapping between old edge indices to new ones.
+         * Deleted edges new index is NO_ID
+         */
+        std::vector< index_t > delete_isolated_edges();
+
+        /*!
+         * Return one polygon with one of the vertices matching given vertex.
+         * @param[in] vertex_id Index of the vertex.
+         * @detail If vertex is isolated (no incident polygon), default
+         * PolygonVertex is returned.
+         */
+        const PolygonVertex& polygon_around_vertex( index_t vertex_id ) const;
+
+        /*!
+         * Set a polygon vertex to a given vertex.
+         * @param[in] polygon_vertex PolygonVertex corresponding to the vertex.
+         * @param[in] vertex_id Index of the vertex.
+         */
+        void associate_polygon_vertex_to_vertex(
+            const PolygonVertex& polygon_vertex, index_t vertex_id );
+
+        void copy(
+            const SurfaceMesh< dimension >& polygonal_surface, BuilderKey )
+        {
+            copy( polygonal_surface );
+        }
+
+    protected:
+        SurfaceMeshBuilder( SurfaceMesh< dimension >& polygonal_surface )
+            : VertexSetBuilder( polygonal_surface ),
+              polygonal_surface_( polygonal_surface )
+        {
+        }
+
+        index_t find_or_create_edge( std::array< index_t, 2 > edge_vertices );
+
+        void copy( const SurfaceMesh< dimension >& polygonal_surface );
+
+    private:
+        virtual void do_set_point(
+            index_t vertex_id, const Point< dimension >& point ) = 0;
+
+        void do_delete_vertices( const std::vector< bool >& to_delete ) final;
+
+        virtual void do_delete_surface_vertices(
+            const std::vector< bool >& to_delete ) = 0;
+
+        virtual void do_set_polygon_vertex(
+            const PolygonVertex& polygon_vertex, index_t vertex_id ) = 0;
+
+        virtual void do_create_polygon(
+            absl::Span< const index_t > vertices ) = 0;
+
+        virtual void do_delete_polygons(
+            const std::vector< bool >& to_delete ) = 0;
+
+        virtual void do_set_polygon_adjacent(
+            const PolygonEdge& polygon_edge, index_t adjacent_id ) = 0;
+
+        std::vector< index_t > delete_edges(
+            const std::vector< bool >& to_delete );
+
+        void update_polygon_vertex(
+            const PolygonVertex& polygon_vertex, index_t vertex_id );
+
+        void update_polygon_vertices( absl::Span< const index_t > old2new );
+
+        void update_edge_vertices( absl::Span< const index_t > old2new );
+
+        void update_edge_vertex( std::array< index_t, 2 > edge_vertices,
+            index_t edge_vertex_id,
+            index_t new_vertex_id );
+
+    private:
+        SurfaceMesh< dimension >& polygonal_surface_;
+    };
+    ALIAS_2D_AND_3D( SurfaceMeshBuilder );
+
+    template < index_t dimension >
+    using SurfaceMeshBuilderFactory = Factory< MeshType,
+        SurfaceMeshBuilder< dimension >,
+        SurfaceMesh< dimension >& >;
+    ALIAS_2D_AND_3D( SurfaceMeshBuilderFactory );
+} // namespace geode
---
+++ include/geode/mesh/core/surface_mesh.h
@@ -0,0 +1,391 @@
+/*
+ * Copyright (c) 2019 - 2020 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <absl/container/inlined_vector.h>
+#include <absl/types/optional.h>
+
+#include <geode/basic/attribute.h>
+#include <geode/basic/bitsery_archive.h>
+#include <geode/basic/factory.h>
+#include <geode/basic/passkey.h>
+#include <geode/basic/pimpl.h>
+
+#include <geode/mesh/common.h>
+#include <geode/mesh/core/vertex_set.h>
+
+namespace geode
+{
+    FORWARD_DECLARATION_DIMENSION_CLASS( Point );
+    FORWARD_DECLARATION_DIMENSION_CLASS( Vector );
+    FORWARD_DECLARATION_DIMENSION_CLASS( BoundingBox );
+    FORWARD_DECLARATION_DIMENSION_CLASS( SurfaceMeshBuilder );
+
+    ALIAS_2D_AND_3D( Vector );
+
+    class AttributeManager;
+
+    struct PolygonEdge;
+} // namespace geode
+
+namespace geode
+{
+    /*!
+     * This struct represents a local vertex in a polygon
+     */
+    struct opengeode_mesh_api PolygonVertex
+    {
+        PolygonVertex() = default;
+        PolygonVertex( index_t polygon_id, index_t vertex_id )
+            : polygon_id( polygon_id ), vertex_id( vertex_id )
+        {
+        }
+        PolygonVertex( PolygonEdge polygon_edge );
+        bool operator==( const PolygonVertex& other ) const
+        {
+            return polygon_id == other.polygon_id
+                   && vertex_id == other.vertex_id;
+        }
+        bool operator!=( const PolygonVertex& other ) const
+        {
+            return !( *this == other );
+        }
+        template < typename Archive >
+        void serialize( Archive& archive )
+        {
+            archive.ext( *this, DefaultGrowable< Archive, PolygonVertex >{},
+                []( Archive& archive, PolygonVertex& polygon_vertex ) {
+                    archive.value4b( polygon_vertex.polygon_id );
+                    archive.value4b( polygon_vertex.vertex_id );
+                } );
+        }
+        index_t polygon_id{ NO_ID };
+        index_t vertex_id{ NO_ID };
+    };
+
+    /*!
+     * This struct represents an edge in a polygon
+     */
+    struct opengeode_mesh_api PolygonEdge
+    {
+        PolygonEdge() = default;
+        PolygonEdge( index_t polygon_id, index_t edge_id )
+            : polygon_id( polygon_id ), edge_id( edge_id )
+        {
+        }
+        PolygonEdge( PolygonVertex polygon_vertex );
+        bool operator==( const PolygonEdge& other ) const
+        {
+            return polygon_id == other.polygon_id && edge_id == other.edge_id;
+        }
+        bool operator!=( const PolygonEdge& other ) const
+        {
+            return !( *this == other );
+        }
+        template < typename Archive >
+        void serialize( Archive& archive )
+        {
+            archive.ext( *this, DefaultGrowable< Archive, PolygonEdge >{},
+                []( Archive& archive, PolygonEdge& polygon_edge ) {
+                    archive.value4b( polygon_edge.polygon_id );
+                    archive.value4b( polygon_edge.edge_id );
+                } );
+        }
+        index_t polygon_id{ NO_ID };
+        index_t edge_id{ NO_ID };
+    };
+
+    using PolygonEdgesOnBorder = absl::InlinedVector< PolygonEdge, 3 >;
+
+    using PolygonsAroundVertex = absl::InlinedVector< PolygonVertex, 10 >;
+
+    /*!
+     * This class represents a Surface made up with polygons (triangles, quads,
+     * ...) of arbitrary dimension and provides mesh functionnalities.
+     */
+    template < index_t dimension >
+    class opengeode_mesh_api SurfaceMesh : public VertexSet
+    {
+        OPENGEODE_DISABLE_COPY( SurfaceMesh );
+        PASSKEY( SurfaceMeshBuilder< dimension >, SurfaceMeshKey );
+
+    public:
+        ~SurfaceMesh();
+
+        const Point< dimension >& point( index_t vertex_id ) const;
+
+        index_t nb_edges() const;
+
+        index_t nb_polygons() const;
+
+        bool isolated_vertex( index_t vertex_id ) const;
+
+        bool isolated_edge( index_t edge_id ) const;
+
+        /*!
+         * Return the number of vertices in a polygon
+         */
+        index_t nb_polygon_vertices( index_t polygon_id ) const;
+
+        /*!
+         * Return the number of edges in a polygon
+         */
+        index_t nb_polygon_edges( index_t polygon_id ) const;
+
+        /*!
+         * Return the index in the mesh of a local vertex in a polygon
+         * @param[in] polygon_vertex Local index of vertex in polygon
+         */
+        index_t polygon_vertex( const PolygonVertex& polygon_vertex ) const;
+
+        /*!
+         * Return the index in the mesh of a local edge in a polygon
+         * @param[in] polygon_edge Local index of edge in polygon
+         */
+        index_t polygon_edge( const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the index in the mesh of a given polygon edge vertex.
+         * @param[in] polygon_edge Local index of edge in a polygon.
+         * @param[in] vertex_id Local index of vertex in the edge (0 or 1).
+         */
+        index_t polygon_edge_vertex(
+            const PolygonEdge& polygon_edge, index_t vertex_id ) const;
+
+        /*!
+         * Return the indices of edge vertices.
+         * @param[in] edge_id Index of an edge.
+         */
+        const std::array< index_t, 2 >& edge_vertices( index_t edge_id ) const;
+
+        /*!
+         * Return the next vertex in a polygon (local indexation)
+         * @param[in] polygon_vertex Local index of vertex in polygon
+         */
+        PolygonVertex next_polygon_vertex(
+            const PolygonVertex& polygon_vertex ) const;
+
+        /*!
+         * Return the previous vertex in a polygon (local indexation)
+         * @param[in] polygon_vertex Local index of vertex in polygon
+         */
+        PolygonVertex previous_polygon_vertex(
+            const PolygonVertex& polygon_vertex ) const;
+
+        /*!
+         * Return the next edge in a polygon (local indexation)
+         * @param[in] polygon_edge Local index of edge in polygon
+         */
+        PolygonEdge next_polygon_edge( const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the previous edge in a polygon (local indexation)
+         * @param[in] polygon_edge Local index of edge in polygon
+         */
+        PolygonEdge previous_polygon_edge(
+            const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the index of the polygon adjacent through an edge.
+         * @param[in] polygon_edge Local index of edge in polygon.
+         * @return the index of the adjacent polygon if it exists.
+         */
+        absl::optional< index_t > polygon_adjacent(
+            const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the index of the edge of the adjacent polygon through which
+         * polygons are adjacent.
+         * @param[in] polygon_edge Local index of edge in polygon.
+         * @return the index of the adjacent polygon edge if it exists.
+         */
+        absl::optional< PolygonEdge > polygon_adjacent_edge(
+            const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return true if the edge is on border, i.e. if the polygon has no
+         * adjacent through the specified edge.
+         * @param[in] polygon_edge Local index of edge in polygon.
+         */
+        bool is_edge_on_border( const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return all the edges of a polygon that are on border
+         * @param[in] polygon_id Index of a polygon
+         */
+        PolygonEdgesOnBorder polygon_edges_on_border(
+            index_t polygon_id ) const;
+
+        /*!
+         * Return the next edge on the border (local indexation).
+         * @param[in] polygon_edge Local index of edge in a polygon.
+         * @pre The given polygon edge should be on border.
+         */
+        PolygonEdge next_on_border( const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the previous edge on the border (local indexation).
+         * @param[in] polygon_edge Local index of edge in a polygon.
+         * @pre The given polygon edge should be on border.
+         */
+        PolygonEdge previous_on_border( const PolygonEdge& polygon_edge ) const;
+
+        /*!
+         * Return the length of a given edge.
+         * @param[in] edge_id Index of edge.
+         */
+        double edge_length( index_t edge_id ) const;
+
+        /*!
+         * Return the coordinates of the barycenter of a given edge.
+         * @param[in] edge_id Index of edge.
+         */
+        Point< dimension > edge_barycenter( index_t edge_id ) const;
+
+        /*!
+         * Return the barycenter of a polygon
+         * @param[in] polygon_id Index of a polygon
+         */
+        Point< dimension > polygon_barycenter( index_t polygon_id ) const;
+
+        /*!
+         * Return the area of a polygon.
+         * @param[in] polygon_id Index of a polygon.
+         * @warning Result guaranteed only for convex polygon.
+         */
+        double polygon_area( index_t polygon_id ) const;
+
+        /*!
+         * Return the normal of a polygon
+         */
+        template < index_t T = dimension >
+        typename std::enable_if< T == 3, Vector3D >::type polygon_normal(
+            index_t polygon_id ) const;
+
+        /*!
+         * Return the normal at a polygon vertex
+         */
+        template < index_t T = dimension >
+        typename std::enable_if< T == 3, Vector3D >::type polygon_vertex_normal(
+            index_t vertex_id ) const;
+
+        /*!
+         * Get all the polygons with one of the vertices matching given vertex.
+         * @param[in] vertex_id Index of the vertex.
+         * @pre This function needs that polygon adjacencies are computed
+         */
+        PolygonsAroundVertex polygons_around_vertex( index_t vertex_id ) const;
+
+        /*!
+         * Find the polygon edge corresponding to an ordered pair of vertex
+         * indices.
+         * @param[in] from_vertex_id Index of the vertex from which starts the
+         * edge
+         * @param[in] to_vertex_id Index of the vertex to which ends the edge
+         * @return Local index if the edge is found.
+         */
+        absl::optional< PolygonEdge > polygon_edge_from_vertices(
+            index_t from_vertex_id, index_t to_vertex_id ) const;
+
+        /*!
+         * Get the index of edge corresponding to given vertices
+         * @param[in] vertices Ordered vertex indices
+         */
+        absl::optional< index_t > edge_from_vertices(
+            const std::array< index_t, 2 >& vertices ) const;
+
+        /*!
+         * Access to the manager of attributes associated with edges.
+         */
+        AttributeManager& edge_attribute_manager() const;
+
+        /*!
+         * Access to the manager of attributes associated with polygons.
+         */
+        AttributeManager& polygon_attribute_manager() const;
+
+        /*!
+         * Compute the bounding box from mesh vertices
+         */
+        BoundingBox< dimension > bounding_box() const;
+
+        const PolygonVertex& polygon_around_vertex(
+            index_t vertex_id, SurfaceMeshKey ) const;
+
+        void associate_polygon_vertex_to_vertex(
+            const PolygonVertex& polygon_vertex,
+            index_t vertex_id,
+            SurfaceMeshKey );
+
+        void update_edge_vertices(
+            absl::Span< const index_t > old2new, SurfaceMeshKey );
+
+        void update_edge_vertex( std::array< index_t, 2 > edge_vertices,
+            index_t edge_vertex_id,
+            index_t new_vertex_id,
+            SurfaceMeshKey );
+
+        void remove_edge(
+            std::array< index_t, 2 > edge_vertices, SurfaceMeshKey );
+
+        std::vector< index_t > delete_edges(
+            const std::vector< bool >& to_delete, SurfaceMeshKey );
+
+        std::vector< index_t > remove_isolated_edges( SurfaceMeshKey );
+
+        index_t find_or_create_edge(
+            std::array< index_t, 2 > edge_vertices, SurfaceMeshKey )
+        {
+            return find_or_create_edge( std::move( edge_vertices ) );
+        }
+
+        void overwrite_edges(
+            const SurfaceMesh< dimension >& from, SurfaceMeshKey );
+
+    protected:
+        SurfaceMesh();
+
+        index_t find_or_create_edge( std::array< index_t, 2 > edge_vertices );
+
+    private:
+        friend class bitsery::Access;
+        template < typename Archive >
+        void serialize( Archive& archive );
+
+        virtual const Point< dimension >& get_point(
+            index_t vertex_id ) const = 0;
+
+        virtual index_t get_polygon_vertex(
+            const PolygonVertex& polygon_vertex ) const = 0;
+
+        virtual index_t get_nb_polygon_vertices( index_t polygon_id ) const = 0;
+
+        virtual absl::optional< index_t > get_polygon_adjacent(
+            const PolygonEdge& polygon_edge ) const = 0;
+
+    private:
+        IMPLEMENTATION_MEMBER( impl_ );
+    };
+    ALIAS_2D_AND_3D( SurfaceMesh );
+} // namespace geode
