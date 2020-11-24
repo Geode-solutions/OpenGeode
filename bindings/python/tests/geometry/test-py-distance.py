e89d5417e5b28722f9d002187df27049b903aecf
panquez <pierre.anquez@geode-solutions.com>
2020-11-24 15:00:43 +0100

    fix

--- bindings/python/tests/geometry/test-py-distance.py
+++ bindings/python/tests/geometry/test-py-distance.py
@@ -21,7 +21,7 @@
 
 import os, sys, platform
 if sys.version_info >= (3,8,0) and platform.system() == "Windows":
-    for path in [x.strip() for x in os.environ['PATH'].split('') if x]:
+    for path in [x.strip() for x in os.environ['PATH'].split(';') if x]:
         os.add_dll_directory(path)
 
 import math
