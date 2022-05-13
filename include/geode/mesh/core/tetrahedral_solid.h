87d2e8f981968842c1089e22b4c7bcc9e11d9254
panquez <pierre.anquez@geode-solutions.com>
2022-05-13 14:27:45 +0200

    fix(Mesh): add more around methods

--- include/geode/mesh/core/tetrahedral_solid.h
+++ include/geode/mesh/core/tetrahedral_solid.h
@@ -75,6 +75,9 @@ namespace geode
             const std::array< index_t, 2 >& vertices ) const final;
 
         PolyhedraAroundEdge polyhedra_around_edge(
+            const PolyhedronFacetEdge& edge ) const final;
+
+        PolyhedraAroundEdge polyhedra_around_edge(
             const std::array< index_t, 2 >& vertices,
             index_t first_polyhedron ) const final;
 
