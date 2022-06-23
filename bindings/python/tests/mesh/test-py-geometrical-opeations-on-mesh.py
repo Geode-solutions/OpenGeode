8dca41158e2366126925adc683e2918a8c702e93
panquez <pierre.anquez@geode-solutions.com>
2022-06-23 14:33:24 +0200

    fix test

--- bindings/python/tests/mesh/test-py-geometrical-opeations-on-mesh.py
+++ bindings/python/tests/mesh/test-py-geometrical-opeations-on-mesh.py
@@ -28,56 +28,6 @@ import opengeode_py_basic as basic
 import opengeode_py_geometry as geom
 import opengeode_py_mesh as mesh
 
-def test_create_vertices( surface, builder ):
-    builder.create_point( geom.Point3D( [ 0.1, 0.2, 0.3 ] ) )
-    builder.create_point( geom.Point3D( [ 2.1, 9.4, 6.7 ] ) )
-    builder.create_point( geom.Point3D( [ 7.5, 5.2, 6.3 ] ) )
-    builder.create_point( geom.Point3D( [ 8.1, 1.4, 4.7 ] ) )
-    builder.create_point( geom.Point3D( [ 4.7, 2.1, 1.3 ] ) )
-    if surface.nb_vertices() != 5:
-        raise ValueError( "[Test] TriangulatedSurface should have 5 vertices" )
-
-def test_create_polygons(  surface, builder ):
-    builder.create_triangle( [ 0, 1, 2 ] )
-    builder.create_triangle( [ 1, 3, 2 ] )
-    builder.create_polygon( [ 3, 4, 2 ] )
-    if surface.nb_polygons() != 3:
-        raise ValueError( "[Test] TriangulatedSurface should have 3 triangles" )
-    if surface.edges().nb_edges() != 7:
-        raise ValueError( "[Test] TriangulatedSurface should have 7 edges" )
-
-def test_polygon_adjacencies( surface, builder ):
-    builder.compute_polygon_adjacencies()
-    if surface.polygon_adjacent( mesh.PolygonEdge( 0, 0 ) ):
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-    if surface.polygon_adjacent( mesh.PolygonEdge( 0, 1 ) ) != 1:
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-    if surface.polygon_adjacent( mesh.PolygonEdge( 1, 2 ) ) != 0:
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-    if surface.polygon_adjacent_edge( mesh.PolygonEdge( 0, 1 ) ) != mesh.PolygonEdge( 1, 2 ):
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-
-    if surface.polygon_adjacent( mesh.PolygonEdge( 2, 2 ) ) != 1:
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-    if surface.polygon_adjacent( mesh.PolygonEdge( 2, 0 ) ):
-        raise ValueError( "[Test] TriangulatedSurface adjacent index is not correct" )
-
-def test_delete_polygon( surface, builder ):
-    to_delete = [False] * surface.nb_polygons()
-    to_delete[0] = True
-    builder.delete_polygons( to_delete )
-    if surface.nb_polygons() != 2:
-        raise ValueError( "[Test] TriangulatedSurface should have 2 polygons" )
-    if surface.polygon_vertex( mesh.PolygonVertex( 0, 0 ) ) != 3:
-        raise ValueError( "[Test] TriangulatedSurface edge vertex index is not correct" )
-    if surface.polygon_vertex( mesh.PolygonVertex( 0, 1 ) ) != 2:
-        raise ValueError( "[Test] TriangulatedSurface edge vertex index is not correct" )
-    if surface.polygon_vertex( mesh.PolygonVertex( 0, 2 ) ) != 1:
-        raise ValueError( "[Test] TriangulatedSurface edge vertex index is not correct" )
-    builder.edges_builder().delete_isolated_edges()
-    if surface.edges().nb_edges() != 5:
-        raise ValueError( "[Test] TriangulatedSurface should have 5 edges" )
-
 def create_surface():
     surface = mesh.TriangulatedSurface3D.create()
     builder = mesh.TriangulatedSurfaceBuilder3D.create( surface )
@@ -94,29 +44,29 @@ def create_surface():
 def test_rescale( surface ):
     builder = mesh.TriangulatedSurfaceBuilder3D.create( surface )
     mesh.rescale_triangulated_surface3D( surface, builder, [ 2, -2, 0.1 ] )
-    if surface.point( 0 ) != [ 0.2, -0.4, 0.03 ]:
+    if not surface.point( 0 ).inexact_equal(geom.Point3D( [ 0.2, -0.4, 0.03 ]), 1e-6  ):
         raise ValueError( "[Test] Wrong rescale of vertex 0" )
-    if surface.point( 1 ) != [ 4.2, -18.8, 0.67 ]:
+    if not surface.point( 1 ).inexact_equal(geom.Point3D( [ 4.2, -18.8, 0.67 ]), 1e-6  ):
         raise ValueError( "[Test] Wrong rescale of vertex 1" )
-    if surface.point( 2 ) != [ 15.0, -10.4, 0.63 ]:
+    if not surface.point( 2 ).inexact_equal(geom.Point3D( [ 15.0, -10.4, 0.63 ]), 1e-6  ):
         raise ValueError( "[Test] Wrong rescale of vertex 2" )
-    if surface.point( 3 ) != [ 16.2, -2.8, 0.47 ]:
+    if not surface.point( 3 ).inexact_equal(geom.Point3D( [ 16.2, -2.8, 0.47 ]), 1e-6  ):
         raise ValueError( "[Test] Wrong rescale of vertex 3" )
-    if surface.point( 4 ) != [ 9.4, -4.2, 0.13 ]:
+    if not surface.point( 4 ).inexact_equal(geom.Point3D( [ 9.4, -4.2, 0.13 ]), 1e-6  ):
         raise ValueError( "[Test] Wrong rescale of vertex 4" )
 
 def test_translate( surface ):
     builder = mesh.TriangulatedSurfaceBuilder3D.create( surface )
-    mesh.translate_triangulated_surface3D( surface, builder, [ 2, -2, 1 ] )
-    if surface.point( 0 ) != [ 2.1, -1.8, 1.3 ]:
+    mesh.translate_triangulated_surface3D( surface, builder, geom.Vector3D( [ 2, -2, 1 ] ) )
+    if not surface.point( 0 ).inexact_equal(geom.Point3D([ 2.1, -1.8, 1.3 ] ) , 1e-6 ):
         raise ValueError( "[Test] Wrong translation of vertex 0" )
-    if surface.point( 1 ) != [ 4.1, 7.4, 7.7 ]:
+    if not surface.point( 1 ).inexact_equal(geom.Point3D([ 4.1, 7.4, 7.7 ] ) , 1e-6 ):
         raise ValueError( "[Test] Wrong translation of vertex 1" )
-    if surface.point( 2 ) != [ 9.5, 3.2, 7.3 ]:
+    if not surface.point( 2 ).inexact_equal(geom.Point3D([ 9.5, 3.2, 7.3 ] ) , 1e-6 ):
         raise ValueError( "[Test] Wrong translation of vertex 2" )
-    if surface.point( 3 ) != [ 10.1, -0.6, 5.7 ]:
+    if not surface.point( 3 ).inexact_equal(geom.Point3D([ 10.1, -0.6, 5.7 ] ) , 1e-6 ):
         raise ValueError( "[Test] Wrong translation of vertex 3" )
-    if surface.point( 4 ) != [ 6.7, 0.1, 2.3 ]:
+    if not surface.point( 4 ).inexact_equal(geom.Point3D([ 6.7, 0.1, 2.3 ] ) , 1e-6 ):
         raise ValueError( "[Test] Wrong translation of vertex 4" )
 
 if __name__ == '__main__':
