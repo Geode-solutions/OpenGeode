6f7977faeea0cf4b4cacb9d1dd37dfe465b76e5a
MelchiorSchuh <melchior.schuh-senlis@geode-solutions.com>
2022-07-19 15:04:43 +0200

    fix(repair_polygon_orientations): Fixed the repair method so that the polygons are repaired with a positive area in 2D.

--- tests/mesh/test-repair-polygon-orientations.cpp
+++ tests/mesh/test-repair-polygon-orientations.cpp
@@ -24,6 +24,8 @@
 #include <geode/basic/assert.h>
 #include <geode/basic/logger.h>
 
+#include <geode/geometry/point.h>
+
 #include <geode/mesh/builder/surface_mesh_builder.h>
 #include <geode/mesh/core/surface_edges.h>
 #include <geode/mesh/core/surface_mesh.h>
@@ -37,7 +39,19 @@ std::unique_ptr< geode::SurfaceMesh2D > build_bad_orientation_surface()
     auto surface = geode::SurfaceMesh2D::create();
     auto builder = geode::SurfaceMeshBuilder2D::create( *surface );
     builder->create_vertices( 12 );
-    builder->create_polygon( { 0, 1, 2 } ); // +
+    builder->set_point( 0, { { 0, 0 } } );
+    builder->set_point( 1, { { 1, 0 } } );
+    builder->set_point( 2, { { 1, 1 } } );
+    builder->set_point( 3, { { 2, 0 } } );
+    builder->set_point( 4, { { 2, 1 } } );
+    builder->set_point( 5, { { 3, 2 } } );
+    builder->set_point( 6, { { 3, 1 } } );
+    builder->set_point( 7, { { 4, 1 } } );
+    builder->set_point( 8, { { 3, 0 } } );
+    builder->set_point( 9, { { 4, 0 } } );
+    builder->set_point( 10, { { 4, 2 } } );
+    builder->set_point( 11, { { 4, 3 } } );
+    builder->create_polygon( { 0, 2, 1 } ); // -
     builder->create_polygon( { 1, 3, 2 } ); // +
     builder->create_polygon( { 2, 4, 3 } ); // -
     builder->create_polygon( { 4, 6, 5 } ); // +
@@ -45,6 +59,7 @@ std::unique_ptr< geode::SurfaceMesh2D > build_bad_orientation_surface()
     builder->create_polygon( { 5, 7, 11 } ); // +
     builder->create_polygon( { 6, 8, 9, 7 } ); // +
     builder->create_polygon( { 9, 7, 11, 10 } ); // -
+    builder->create_polygon( { 2, 4, 5 } ); // +
     builder->compute_polygon_adjacencies();
     return surface;
 }
@@ -96,10 +111,10 @@ void check_repaired_surface( const geode::SurfaceMesh2D& surface )
     check_adjacents( surface, 1, { geode::NO_ID, 2, 0 } );
 
     check_vertices_order( surface, 2, { 2, 3, 4 } );
-    check_adjacents( surface, 2, { 1, geode::NO_ID, geode::NO_ID } );
+    check_adjacents( surface, 2, { 1, geode::NO_ID, 8 } );
 
     check_vertices_order( surface, 3, { 4, 6, 5 } );
-    check_adjacents( surface, 3, { geode::NO_ID, 4, geode::NO_ID } );
+    check_adjacents( surface, 3, { geode::NO_ID, 4, 8 } );
 
     check_vertices_order( surface, 4, { 5, 6, 7 } );
     check_adjacents( surface, 4, { 3, 6, 5 } );
@@ -112,6 +127,9 @@ void check_repaired_surface( const geode::SurfaceMesh2D& surface )
 
     check_vertices_order( surface, 7, { 9, 10, 11, 7 } );
     check_adjacents( surface, 7, { geode::NO_ID, geode::NO_ID, 5, 6 } );
+
+    check_vertices_order( surface, 8, { 2, 4, 5 } );
+    check_adjacents( surface, 8, { 2, 3, geode::NO_ID } );
 }
 
 absl::FixedArray< std::array< geode::index_t, 2 > > get_edges(
