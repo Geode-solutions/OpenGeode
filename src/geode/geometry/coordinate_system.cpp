20e6d416b17dc9558212ed9a6630a7a13bb85f07
panquez <pierre.anquez@geode-solutions.com>
2023-06-27 09:32:36 +0200

    fix(CoordinateSystem): add 3D version of coordinates computation

--- src/geode/geometry/coordinate_system.cpp
+++ src/geode/geometry/coordinate_system.cpp
@@ -26,9 +26,42 @@
 #include <geode/basic/logger.h>
 
 #include <geode/geometry/basic_objects/infinite_line.h>
+#include <geode/geometry/basic_objects/plane.h>
+#include <geode/geometry/basic_objects/triangle.h>
 #include <geode/geometry/distance.h>
 #include <geode/geometry/information.h>
+#include <geode/geometry/perpendicular.h>
 #include <geode/geometry/position.h>
+
+namespace
+{
+    void check_frame( std::array< geode::Vector2D, 2 > directions )
+    {
+        const auto dot =
+            geode::dot_perpendicular( directions[0], directions[1] );
+        OPENGEODE_EXCEPTION( dot > geode::global_angular_epsilon,
+            "[CoordinateSystem2D] Could not create a "
+            "CoordinateSystem with given directions" );
+    }
+
+    void check_frame( std::array< geode::Vector3D, 3 > directions )
+    {
+        for( const auto d0 : geode::LRange{ 2 } )
+        {
+            for( const auto d1 : geode::LRange{ d0 + 1, 3 } )
+            {
+                const auto vector0 = directions[d0].normalize();
+                const auto vector1 = directions[d1].normalize();
+
+                const auto normal = vector0.cross( vector1 );
+                const auto length = normal.length();
+                OPENGEODE_EXCEPTION( length > geode::global_angular_epsilon,
+                    "[CoordinateSystem3D] Could not create a "
+                    "CoordinateSystem with given directions" );
+            }
+        }
+    }
+} // namespace
 
 namespace geode
 {
@@ -44,6 +77,7 @@ namespace geode
         : Frame< dimension >( std::move( directions ) ),
           origin_( std::move( origin ) )
     {
+        check_frame( directions );
     }
 
     template < index_t dimension >
@@ -75,11 +109,28 @@ namespace geode
 
     template <>
     Point< 3 > opengeode_geometry_api CoordinateSystem< 3 >::coordinates(
-        const Point< 3 >& /*unused*/ ) const
+        const Point< 3 >& global_coordinates ) const
     {
-        throw OpenGeodeException{
-            "[CoordinateSystem3D::coordinates] Not implemented yet"
-        };
+        const auto A = origin_ + this->direction( 0 );
+        const auto B = origin_ + this->direction( 1 );
+        const auto C = origin_ + this->direction( 2 );
+        const auto OAB_plane = Triangle3D{ origin_, A, B }.plane().value();
+        const auto OBC_plane = Triangle3D{ origin_, B, C }.plane().value();
+        const auto OCA_plane = Triangle3D{ origin_, C, A }.plane().value();
+        const auto distance_A =
+            std::get< 0 >( point_plane_distance( A, OBC_plane ) );
+        const auto distance_AP = std::get< 0 >(
+            point_plane_signed_distance( global_coordinates, OBC_plane ) );
+        const auto distance_B =
+            std::get< 0 >( point_plane_distance( B, OCA_plane ) );
+        const auto distance_BP = std::get< 0 >(
+            point_plane_signed_distance( global_coordinates, OCA_plane ) );
+        const auto distance_C =
+            std::get< 0 >( point_plane_distance( C, OAB_plane ) );
+        const auto distance_CP = std::get< 0 >(
+            point_plane_signed_distance( global_coordinates, OAB_plane ) );
+        return { { distance_AP / distance_A, distance_BP / distance_B,
+            distance_CP / distance_C } };
     }
 
     template <>
@@ -90,19 +141,15 @@ namespace geode
         const auto B = origin_ + this->direction( 1 );
         const Vector2D OP{ origin_, global_coordinates };
         const InfiniteLine2D OP_line{ OP, origin_ };
-        const auto distance_A = point_line_distance( A, OP_line );
-        const auto distance_B = point_line_distance( B, OP_line );
-        const auto ratio = distance_A / ( distance_A + distance_B );
         const InfiniteLine2D OA_line{ this->direction( 0 ), origin_ };
         const InfiniteLine2D OB_line{ this->direction( 1 ), origin_ };
-        const auto sideB = point_side_to_line( global_coordinates, OA_line );
-        const auto sideA = point_side_to_line( global_coordinates, OB_line );
-        const auto sign_A = sideA == Side::positive ? -1. : 1.;
-        const auto sign_B = sideB == Side::negative ? -1. : 1.;
-        const auto direction = this->direction( 1 ) * ratio * sign_A
-                               + this->direction( 0 ) * ( 1. - ratio ) * sign_B;
-        const auto scale = OP.length() / direction.length();
-        return { { ( 1. - ratio ) * scale * sign_A, ratio * scale * sign_B } };
+        const auto distance_A = point_line_distance( A, OB_line );
+        const auto distance_AP =
+            point_line_signed_distance( global_coordinates, OB_line );
+        const auto distance_B = point_line_distance( B, OA_line );
+        const auto distance_BP =
+            point_line_signed_distance( global_coordinates, OA_line );
+        return { { -distance_AP / distance_A, distance_BP / distance_B } };
     }
 
     template < index_t dimension >
