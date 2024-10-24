8a440aadd23f761e87976535683eccda6e26486a
BotellaA <BotellaA@users.noreply.github.com>
2024-10-24 07:03:21 +0000

    Apply prepare changes

--- include/geode/basic/cached_value.hpp
+++ include/geode/basic/cached_value.hpp
@@ -80,14 +80,15 @@ namespace geode
         template < typename Archive >
         void serialize( Archive& archive )
         {
-            archive.ext( *this, Growable< Archive, CachedValue >{
-                { []( Archive& a, CachedValue& value ) {
-                a.value1b( value.computed_ );
-                a( value.value_ );
-                                    }, [] (Archive& /*a*/, CachedValue& /*value*/ )
-                    { 
-// skip serialize
-} } } );
+            archive.ext(
+                *this, Growable< Archive, CachedValue >{
+                           { []( Archive& a, CachedValue& value ) {
+                                a.value1b( value.computed_ );
+                                a( value.value_ );
+                            },
+                               []( Archive& /*a*/, CachedValue& /*value*/ ) {
+                                   // skip serialize
+                               } } } );
         }
 
     private:
