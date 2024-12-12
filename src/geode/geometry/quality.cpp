79e2b42fc6e77fdac997c489cde0efa0bb51cb39
benoitTHEBAULT <benoit.thebault@geode-solutions.com>
2024-12-12 11:12:13 +0100

    remove constexpr sqrt

--- src/geode/geometry/quality.cpp
+++ src/geode/geometry/quality.cpp
@@ -31,36 +31,6 @@
 #include <geode/geometry/mensuration.hpp>
 #include <geode/geometry/vector.hpp>
 
-namespace
-{
-
-    namespace Detail
-    {
-        double constexpr sqrtNewtonRaphson( double x, double curr, double prev )
-        {
-            return curr == prev ? curr
-                                : sqrtNewtonRaphson(
-                                      x, 0.5 * ( curr + x / curr ), curr );
-        }
-    } // namespace Detail
-
-    /*
-     * Constexpr version of the square root
-     * Return value:
-     *	- For a finite and non-negative value of "x", returns an approximation
-     *for the square root of "x"
-     *   - Otherwise, returns NaN
-     */
-    double constexpr sqrt( double x )
-    {
-        return x >= 0 && x < std::numeric_limits< double >::infinity()
-                   ? Detail::sqrtNewtonRaphson( x, x, 0 )
-                   : std::numeric_limits< double >::quiet_NaN();
-    }
-
-    constexpr double SIX_SQRT_2 = 6 * sqrt( 2 );
-} // namespace
-
 namespace geode
 {
     double tetrahedron_aspect_ratio( const Tetrahedron& tetra )
