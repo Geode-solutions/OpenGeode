0230d6eea6da2b7cf5253e747b7e0b6def7cbb47
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-02-21 09:23:45 +0100

    feat(SmallSet): new set class optimize for small capacity

---
+++ include/geode/basic/small_set.hpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2019 - 2025 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <absl/algorithm/container.h>
+#include <absl/container/inlined_vector.h>
+
+#include <geode/basic/common.hpp>
+#include <geode/basic/range.hpp>
+
+namespace geode
+{
+    template < typename Type, index_t capacity = 10 >
+    class SmallSet
+    {
+    public:
+        auto size() const
+        {
+            return container_.size();
+        }
+
+        auto empty() const
+        {
+            return container_.empty();
+        }
+
+        auto begin() const
+        {
+            return container_.begin();
+        }
+
+        auto end() const
+        {
+            return container_.end();
+        }
+
+        auto insert( const Type& element )
+        {
+            if( absl::c_contains( container_, element ) )
+            {
+                return false;
+            }
+            container_.push_back( element );
+            return true;
+        }
+
+    private:
+        absl::InlinedVector< Type, capacity > container_;
+    };
+} // namespace geode
---
+++ tests/basic/test-small-set.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2019 - 2025 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/basic/logger.hpp>
+#include <geode/basic/small_set.hpp>
+
+#include <geode/tests/common.hpp>
+
+void test()
+{
+    geode::OpenGeodeBasicLibrary::initialize();
+    geode::Logger::set_level( geode::Logger::LEVEL::debug );
+    geode::SmallSet< double > set;
+    OPENGEODE_EXCEPTION( set.empty(), "[Test] Set should be empty" );
+    OPENGEODE_EXCEPTION( set.insert( 0 ), "[Test] Insert should be done" );
+    OPENGEODE_EXCEPTION( set.insert( 1 ), "[Test] Insert should be done" );
+    OPENGEODE_EXCEPTION( !set.insert( 0 ), "[Test] Insert not allow" );
+    OPENGEODE_EXCEPTION( !set.insert( 1 ), "[Test] Insert not allow" );
+    OPENGEODE_EXCEPTION( set.size(), "[Test] Set size should be 2" );
+}
+
+OPENGEODE_TEST( "small-set" )
