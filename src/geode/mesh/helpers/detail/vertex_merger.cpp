aaf1fef8b12fe3564eb9bd1389b59f6693024236
BenoitTHEBAULT <86229235+BenoitTHEBAULT@users.noreply.github.com>
2025-03-12 09:13:40 +0000

    Apply prepare changes

--- src/geode/mesh/helpers/detail/vertex_merger.cpp
+++ src/geode/mesh/helpers/detail/vertex_merger.cpp
@@ -218,8 +218,8 @@ namespace geode
         }
 
         template < typename Mesh >
-        auto VertexMerger< Mesh >::vertex_origins( index_t vertex ) const
-            -> const VertexOrigins&
+        auto VertexMerger< Mesh >::vertex_origins(
+            index_t vertex ) const -> const VertexOrigins&
         {
             return impl_->vertex_origins( vertex );
         }
