90801e95a54906469c8e6870b0e2db9a4b1c1bc1
benoitTHEBAULT <benoit.thebault@geode-solutions.com>
2025-03-21 10:39:02 +0100

    feat(AnisotropicDistanceToSurface): adding DistanceToSurface for anistropic cases.

--- src/geode/mesh/helpers/aabb_surface_helpers.cpp
+++ src/geode/mesh/helpers/aabb_surface_helpers.cpp
@@ -33,6 +33,7 @@
 
 #include <geode/geometry/aabb.hpp>
 #include <geode/geometry/basic_objects/triangle.hpp>
+#include <geode/geometry/coordinate_system.hpp>
 #include <geode/geometry/distance.hpp>
 #include <geode/geometry/point.hpp>
 
@@ -67,6 +68,23 @@ namespace geode
             point_triangle_distance( query, mesh_.triangle( cur_box ) ) );
     }
 
+    template < index_t dimension >
+    double AnisotropicDistanceToTriangle< dimension >::operator()(
+        const Point< dimension >& query, index_t cur_box ) const
+    {
+        const auto triangle = mesh_.triangle( cur_box );
+        const auto vertices = triangle.vertices();
+        const auto point0 = coordinate_system_.coordinates( vertices[0].get() );
+        const auto point1 = coordinate_system_.coordinates( vertices[1].get() );
+        const auto point2 = coordinate_system_.coordinates( vertices[2].get() );
+        const Triangle< dimension > triangle_in_metric_space{ point0, point1,
+            point2 };
+        const auto query_in_metric_space =
+            coordinate_system_.coordinates( query );
+        return std::get< 0 >( point_triangle_distance(
+            query_in_metric_space, triangle_in_metric_space ) );
+    }
+
     template opengeode_mesh_api AABBTree2D create_aabb_tree< 2 >(
         const SurfaceMesh2D& );
     template opengeode_mesh_api AABBTree3D create_aabb_tree< 3 >(
@@ -74,5 +92,7 @@ namespace geode
 
     template class opengeode_mesh_api DistanceToTriangle< 2 >;
     template class opengeode_mesh_api DistanceToTriangle< 3 >;
+    template class opengeode_mesh_api AnisotropicDistanceToTriangle< 2 >;
+    template class opengeode_mesh_api AnisotropicDistanceToTriangle< 3 >;
 
 } // namespace geode
