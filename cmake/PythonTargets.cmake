5fd71bb3ee2a915e5b19fccf89ded3fded99bc2f
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-09-04 19:46:47 +0200

    Update PythonTargets.cmake

--- cmake/PythonTargets.cmake
+++ cmake/PythonTargets.cmake
@@ -147,6 +147,6 @@ print(name + version + '-' + name + version + '-' + platform)"
     string(CONCAT import_test "import " "${project_name}")
     add_custom_target(test-wheel
         COMMAND ${venv_path}/bin/pip install --force-reinstall --no-deps ${wheel_file}
-        COMMAND $${venv_path}/bin/python -c ${import_test}
+        COMMAND ${venv_path}/bin/python -c ${import_test}
     )
 endfunction()
