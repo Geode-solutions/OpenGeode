1a204c47149a6871fd8f75781e9e79c599abc9e3
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-09-11 09:22:22 +0200

    pip install --no-deps .

--- cmake/PythonTargets.cmake
+++ cmake/PythonTargets.cmake
@@ -144,7 +144,7 @@ print(name + version + '-' + name + version + '-' + platform)"
         COMMAND ${CMAKE_COMMAND} -E copy_directory "${wheel_build_directory}/${binary_folder}/${wheel_config_folder}" "${wheel_output_directory}/${binary_folder}"
         COMMAND ${CMAKE_COMMAND} -E copy_directory "${wheel_build_directory}/share" "${wheel_output_directory}/share"
         COMMAND ${CMAKE_COMMAND} -E remove "${wheel_output_directory}/${binary_folder}/*.py"
-        COMMAND ${venv_bin}/pip install .
+        COMMAND ${venv_bin}/pip install --no-deps .
         COMMAND ${venv_bin}/pybind11-stubgen --exit-code -o "${wheel_output_directory}" "${project_name}"
         COMMAND ${venv_bin}/python -m build
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/wheel
