bf4abce8e8fd247c2b17c8741000e5063f6af0d1
Pierre Anquez <pierre.anquez@geode-solutions.com>
2025-12-16 11:02:18 +0100

    fix

--- src/geode/mesh/core/surface_mesh.cpp
+++ src/geode/mesh/core/surface_mesh.cpp
@@ -502,7 +502,7 @@ namespace geode
                 const std::optional< PolygonVertex >& first_polygon ) const
         {
             const auto& cached = polygons_around_vertex_->value( vertex_id );
-            if( !cached.computed()
+            if( cached.computed()
                 && ( first_polygon
                      && absl::c_contains(
                          cached.value().polygons, first_polygon.value() ) ) )
