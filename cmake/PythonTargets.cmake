1ada6425edfda2ed70104263887c8b6f3670b3b1
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-03-25 14:38:08 +0100

    fix(Wheel): upgrade pip

--- cmake/PythonTargets.cmake
+++ cmake/PythonTargets.cmake
@@ -111,7 +111,7 @@ function(add_geode_python_wheel)
         set(venv_bin "${venv_path}/Scripts")
     endif()
     execute_process(
-        COMMAND ${venv_bin}/pip install --upgrade wheel packaging setuptools build pybind11-stubgen
+        COMMAND ${venv_bin}/pip install --upgrade pip wheel packaging setuptools build pybind11-stubgen
     )
     execute_process(
         COMMAND ${venv_bin}/python -c 
