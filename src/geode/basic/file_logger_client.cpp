836a3487ead145167b2e388748d4ad79f3427965
Francois Bonneau <francois.bonneau@geode-solutions.com>
2026-05-18 14:39:45 +0200

    typo

--- src/geode/basic/file_logger_client.cpp
+++ src/geode/basic/file_logger_client.cpp
@@ -51,10 +51,10 @@ namespace geode
 
         void set_file_path( std::string_view file_path )
         {
-            static constexpr auto logger_name = "geode_logger_file";
-            spdlog::drop( logger_name );
+            static constexpr auto LOGGER_NAME = "geode_logger_file";
+            spdlog::drop( LOGGER_NAME );
             logger_impl_ = spdlog::basic_logger_mt(
-                logger_name, std::string( file_path ) );
+                LOGGER_NAME, std::string( file_path ) );
             if( always_flush_ )
             {
                 always_flush();
