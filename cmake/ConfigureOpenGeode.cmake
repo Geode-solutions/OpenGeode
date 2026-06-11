6f90268bcaf223a15ee8e17d4a1e22478d03f479
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-06-11 16:32:43 +0200

    remove symlink

--- cmake/ConfigureOpenGeode.cmake
+++ cmake/ConfigureOpenGeode.cmake
@@ -95,15 +95,4 @@ if(OPENGEODE_WITH_PYTHON OR INCLUDE_PYBIND11)
     add_dependencies(download pybind11-download)
 endif()
 
-include(GNUInstallDirs)
-add_custom_target(post_install 
-    ALL
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${OpenGeode_PATH_INSTALL}/bin ${PROJECT_BINARY_DIR}/bin
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${OpenGeode_PATH_INSTALL}/${CMAKE_INSTALL_LIBDIR} ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${OpenGeode_PATH_INSTALL}/cmake ${PROJECT_BINARY_DIR}/cmake
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${OpenGeode_PATH_INSTALL}/include ${PROJECT_BINARY_DIR}/include
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${OpenGeode_PATH_INSTALL}/share ${PROJECT_BINARY_DIR}/share
-    DEPENDS
-        opengeode)
-
 install(DIRECTORY ${OpenGeode_PATH_INSTALL}/ DESTINATION .)
